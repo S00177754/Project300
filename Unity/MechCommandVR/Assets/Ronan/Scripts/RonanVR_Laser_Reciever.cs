1541a32365653e6f5ecc71fc4915537080f27f9c
Ronan Cassidy <36006416+S00177754@users.noreply.github.com>
2019-10-15 14:48:40 +0100

    Controller Laser Pointer Interaction
    Wrote a script which allows steam vr laser pointer to interact with both UI and  game objects based on their name.

---
+++ Unity/MechCommandVR/Assets/Ronan/Scripts/RonanVR_Laser_Reciever.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.Extras;
+
+public class RonanVR_Laser_Reciever : MonoBehaviour
+{
+    public SteamVR_LaserPointer laserPointer;
+
+    void Awake()
+    {
+        laserPointer.PointerIn += PointerInside;  
+        laserPointer.PointerOut += PointerOutside;  
+        laserPointer.PointerClick += PointerClick;  
+    }
+
+    //Pointer Interaction Event Handlers
+    //Remember to attatch a box collider to UI elements so the laser hits it, collide can be set to trigger
+    private void PointerClick(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "TestButton")
+        {
+            Debug.Log("TestButton was clicked");
+        }
+    }
+
+    private void PointerOutside(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "TestButton")
+        {
+            Debug.Log("TestButton was exited");
+        }
+    }
+
+    private void PointerInside(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "TestButton")
+        {
+            Debug.Log("TestButton was entered");
+        }
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
