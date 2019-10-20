ebd65dc8da6960c55ac83c24892402d90cf79b22
Ronan Cassidy <36006416+S00177754@users.noreply.github.com>
2019-10-20 12:08:47 +0100

    Unit Selection Screen partial implementation
    -Partial implementation of unit selection buttons

---
+++ Unity/MechCommandVR/Assets/Ronan/Scripts/UnitDetailButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitDetailButton : MonoBehaviour
+{
+    public Image imgUnit;
+    public Image imgType;
+    public TextMeshProUGUI tmpTxtName;
+    public TextMeshProUGUI tmpTxtHealth;
+
+    public void InitializeUnitButton()
+    {
+
+    }
+}
---
+++ Unity/MechCommandVR/Assets/Ronan/Scripts/UnitPanel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitPanel : MonoBehaviour
+{
+    public RectTransform lstGridUnits;
+    public GameObject UnitDetailPrefab;
+    public List<int> unitIdList;
+    public int amount;
+
+    void Start()
+    {
+        LoadList();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void LoadList()
+    {
+        if(unitIdList != null)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                GameObject button = Instantiate(UnitDetailPrefab, lstGridUnits);
+
+                UnitDetailButton gridButton = button.GetComponent<UnitDetailButton>();
+                gridButton.InitializeUnitButton();
+                Debug.Log("Button created");
+            }
+        }
+    }
+
+
+
+
+            //gridButton.Selected += GridButton_OnSelected;
+            //gridButton.Clicked += GridButton_OnClicked;
+        
+    
+}
