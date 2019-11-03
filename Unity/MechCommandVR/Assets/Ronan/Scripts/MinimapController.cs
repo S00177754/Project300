3685f9605f60d4ca0edb4d46bc7256762ca9882e
Ronan Cassidy <36006416+S00177754@users.noreply.github.com>
2019-11-03 02:27:08 +0000

    Minimap Layering Created
    -Minimap layer created with ability for cameras to not see minimap icons

---
+++ Unity/MechCommandVR/Assets/Ronan/Scripts/MinimapController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinimapController : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  
+    public void DebugClick()
+    {
+        Debug.Log("Clicked");
+
+      
+    }
+}
