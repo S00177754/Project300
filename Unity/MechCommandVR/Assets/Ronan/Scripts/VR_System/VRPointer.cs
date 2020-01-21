0e000bf7cdd3a7cf582f1a10191dce144f51f9a1
Ronan Cassidy <36006416+S00177754@users.noreply.github.com>
2020-01-21 14:51:37 +0000

    VR Input Partial Update
    - A few bugs and fixes to impliment but doess handle clicking on both colliders and UI together

---
+++ Unity/MechCommandVR/Assets/Ronan/Scripts/VR_System/VRPointer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class VRPointer : MonoBehaviour
+{
+    public float DefaultLength = 5.0f;
+    public GameObject PointerEnd;
+    public VRInput VR_Input;
+
+    private LineRenderer LineRenderer = null;
+
+    void Awake()
+    {
+        LineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        UpdateLineRenderer();
+    }
+
+    private void UpdateLineRenderer()
+    {
+        //Check for distance from input if none then use default
+        PointerEventData data = VR_Input.GetData();
+        //float targetLength = DefaultLength;
+        float targetLength = data.pointerCurrentRaycast.distance == 0 ? DefaultLength : data.pointerCurrentRaycast.distance;
+
+        //Raycast
+        RaycastHit hit = CreateRaycast(targetLength);
+
+        //Default
+        Vector3 endPosition = transform.position + (transform.forward * targetLength);
+
+        //Or based on hit
+        if(hit.collider != null)
+        {
+            endPosition = hit.point;
+        }
+
+        //Set position of pointer end
+        PointerEnd.transform.position = endPosition;
+
+        //Set linerender
+        LineRenderer.SetPosition(0, transform.position);
+        LineRenderer.SetPosition(1, endPosition);
+    }
+
+    private RaycastHit CreateRaycast(float length)
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        Physics.Raycast(ray, out hit, DefaultLength);
+
+        return hit;
+    }
+}
