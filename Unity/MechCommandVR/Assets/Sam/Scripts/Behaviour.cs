83d9f339e3b5cbf88b272db2d93fdaa276343496
Sam ColvilleRea <S00182360@mail.itsligo.ie>
2020-01-23 22:48:18 +0000

    setup admin

---
+++ Unity/MechCommandVR/Assets/Sam/Scripts/Behaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UnitState
+{
+    SCOUTING,
+    ATTACKING,
+    IDLE
+}
+
+public class Behaviour : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Unity/MechCommandVR/Assets/Sam/Scripts/UnitComponent.cs
+++ Unity/MechCommandVR/Assets/Sam/Scripts/UnitComponent.cs
@@ -3,197 +3,197 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class UnitComponent : MonoBehaviour
-//{
-//    //public string Name;
-////    public UnitType myType;
-    
-//////    public float HealthPoints;
-////    //public float AttackPower;
-////    //public float AttackModifier;
-////    public int Level;
+public class UnitComponent : MonoBehaviour
+{
+    public string Name;
+    public UnitType myType;
+
+    public float HealthPoints;
+    public float AttackPower;
+    public float AttackModifier;
+    public int Level;
 
 
 
-//    //public float SignModifier(UnitType enemySign)
-//    //{
-//    //    switch (myType)
-//    //    {
-//    //        case UnitType.Rock:
-//    //            switch (enemySign)
-//    //            {
-//    //                case UnitType.Rock:
-//    //                    return 1.0f; //Draw
-//    //                case UnitType.Paper:
-//    //                    return 0.75f; //Loose
-//    //                case UnitType.Scissor:
-//    //                    return 1.25f; //Win
-//    //                case UnitType.Lizard:
-//    //                    return 1.25f; //Win
-//    //                case UnitType.Spock:
-//    //                    return 0.75f; //Loose
-//    //                default:
-//    //                    return 0;
-//    //            }
-//    //        case UnitType.Paper:
-//    //            switch (enemySign)
-//    //            {
-//    //                case UnitType.Rock:
-//    //                    return 1.25f; //Win
-//    //                case UnitType.Paper:
-//    //                    return 1.0f; //Draw
-//    //                case UnitType.Scissor:
-//    //                    return 0.75f; //Loose
-//    //                case UnitType.Lizard:
-//    //                    return 0.75f; //Loose
-//    //                case UnitType.Spock:
-//    //                    return 1.25f; //Draw
-//    //                default:
-//    //                    return 0;
-//    //            }
-//    //        case UnitType.Scissor:
-//    //            switch (enemySign)
-//    //            {
-//    //                case UnitType.Rock:
-//    //                    return 0.75f; //Loose
-//    //                case UnitType.Paper:
-//    //                    return 1.25f; //Win
-//    //                case UnitType.Scissor:
-//    //                    return 1.0f; //Draw
-//    //                case UnitType.Lizard:
-//    //                    return 1.25f; //Win
-//    //                case UnitType.Spock:
-//    //                    return 0.75f; //Loose
-//    //                default:
-//    //                    return 0;
-//    //            }
-//    //        case UnitType.Lizard:
-//    //            switch (enemySign)
-//    //            {
-//    //                case UnitType.Rock:
-//    //                    return 0.75f; //Loose
-//    //                case UnitType.Paper:
-//    //                    return 1.25f; //Win
-//    //                case UnitType.Scissor:
-//    //                    return 0.75f; //Loose
-//    //                case UnitType.Lizard:
-//    //                    return 1.0f; //Draw
-//    //                case UnitType.Spock:
-//    //                    return 1.25f; //Win
-//    //                default:
-//    //                    return 0;
-//    //            }
-//    //        case UnitType.Spock:
-//    //            switch (enemySign)
-//    //            {
-//    //                case UnitType.Rock:
-//    //                    return 1.25f; //Win 
-//    //                case UnitType.Paper:
-//    //                    return 0.75f; //Loose
-//    //                case UnitType.Scissor:
-//    //                    return 1.25f; //Win
-//    //                case UnitType.Lizard:
-//    //                    return 0.75f; //Loose
-//    //                case UnitType.Spock:
-//    //                    return 1.0f; //Draw
-//    //                default:
-//    //                    return 0;
-//    //            }
-//    //        default:
-//    //            return 0;
-//    //    }
-//    //}
+    public float SignModifier(UnitType enemySign)
+    {
+        switch (myType)
+        {
+            case UnitType.Rock:
+                switch (enemySign)
+                {
+                    case UnitType.Rock:
+                        return 1.0f; //Draw
+                    case UnitType.Paper:
+                        return 0.75f; //Loose
+                    case UnitType.Scissor:
+                        return 1.25f; //Win
+                    case UnitType.Lizard:
+                        return 1.25f; //Win
+                    case UnitType.Spock:
+                        return 0.75f; //Loose
+                    default:
+                        return 0;
+                }
+            case UnitType.Paper:
+                switch (enemySign)
+                {
+                    case UnitType.Rock:
+                        return 1.25f; //Win
+                    case UnitType.Paper:
+                        return 1.0f; //Draw
+                    case UnitType.Scissor:
+                        return 0.75f; //Loose
+                    case UnitType.Lizard:
+                        return 0.75f; //Loose
+                    case UnitType.Spock:
+                        return 1.25f; //Draw
+                    default:
+                        return 0;
+                }
+            case UnitType.Scissor:
+                switch (enemySign)
+                {
+                    case UnitType.Rock:
+                        return 0.75f; //Loose
+                    case UnitType.Paper:
+                        return 1.25f; //Win
+                    case UnitType.Scissor:
+                        return 1.0f; //Draw
+                    case UnitType.Lizard:
+                        return 1.25f; //Win
+                    case UnitType.Spock:
+                        return 0.75f; //Loose
+                    default:
+                        return 0;
+                }
+            case UnitType.Lizard:
+                switch (enemySign)
+                {
+                    case UnitType.Rock:
+                        return 0.75f; //Loose
+                    case UnitType.Paper:
+                        return 1.25f; //Win
+                    case UnitType.Scissor:
+                        return 0.75f; //Loose
+                    case UnitType.Lizard:
+                        return 1.0f; //Draw
+                    case UnitType.Spock:
+                        return 1.25f; //Win
+                    default:
+                        return 0;
+                }
+            case UnitType.Spock:
+                switch (enemySign)
+                {
+                    case UnitType.Rock:
+                        return 1.25f; //Win 
+                    case UnitType.Paper:
+                        return 0.75f; //Loose
+                    case UnitType.Scissor:
+                        return 1.25f; //Win
+                    case UnitType.Lizard:
+                        return 0.75f; //Loose
+                    case UnitType.Spock:
+                        return 1.0f; //Draw
+                    default:
+                        return 0;
+                }
+            default:
+                return 0;
+        }
+    }
 
-//    //public float LevelModifier(UnitComponent enemy)
-//    //{
-//    //    switch (Level)
-//    //    {
-//    //        case 1:
-//    //            switch (enemy.Level)
-//    //            {
-//    //                case 1:
-//    //                    return 1;
-//    //                case 2:
-//    //                    return 0.85f;
-//    //                case 3:
-//    //                    return 0.6f;
-//    //                case 4:
-//    //                    return 0.5f;
-//    //                default:
-//    //                    return 0.3f;
-//    //            }
-//    //        case 2:
-//    //            switch (enemy.Level)
-//    //            {
-//    //                case 1:
-//    //                    return 1.15f;
-//    //                case 2:
-//    //                    return 1f;
-//    //                case 3:
-//    //                    return 0.85f;
-//    //                case 4:
-//    //                    return 0.6f;
-//    //                default:
-//    //                    return 0.5f;
-//    //            }
-//    //        case 3:
-//    //            switch (enemy.Level)
-//    //            {
-//    //                case 1:
-//    //                    return 1.3f;
-//    //                case 2:
-//    //                    return 1.15f;
-//    //                case 3:
-//    //                    return 1f;
-//    //                case 4:
-//    //                    return 0.85f;
-//    //                default:
-//    //                    return 0.6f;
-//    //            }
-//    //        case 4:
-//    //            switch (enemy.Level)
-//    //            {
-//    //                case 1:
-//    //                    return 1.5f;
-//    //                case 2:
-//    //                    return 1.3f;
-//    //                case 3:
-//    //                    return 1.15f;
-//    //                case 4:
-//    //                    return 1f;
-//    //                default:
-//    //                    return 0.85f;
-//    //            }
-//    //        case 5:
-//    //            switch (enemy.Level)
-//    //            {
-//    //                case 1:
-//    //                    return 1.6f;
-//    //                case 2:
-//    //                    return 1.5f;
-//    //                case 3:
-//    //                    return 1.3f;
-//    //                case 4:
-//    //                    return 1.15f;
-//    //                default:
-//    //                    return 1f;
-//    //            }
-//    //        default:
-//    //            return 1;
-//    //    }
-//    //}
-
-
-//    void Start()
-//    {
-//        //HealthPoints = 1;
-//        //AttackPower = 0.1f;
-//        //AttackModifier = 1;
-//    }
+    public float LevelModifier(UnitComponent enemy)
+    {
+        switch (Level)
+        {
+            case 1:
+                switch (enemy.Level)
+                {
+                    case 1:
+                        return 1;
+                    case 2:
+                        return 0.85f;
+                    case 3:
+                        return 0.6f;
+                    case 4:
+                        return 0.5f;
+                    default:
+                        return 0.3f;
+                }
+            case 2:
+                switch (enemy.Level)
+                {
+                    case 1:
+                        return 1.15f;
+                    case 2:
+                        return 1f;
+                    case 3:
+                        return 0.85f;
+                    case 4:
+                        return 0.6f;
+                    default:
+                        return 0.5f;
+                }
+            case 3:
+                switch (enemy.Level)
+                {
+                    case 1:
+                        return 1.3f;
+                    case 2:
+                        return 1.15f;
+                    case 3:
+                        return 1f;
+                    case 4:
+                        return 0.85f;
+                    default:
+                        return 0.6f;
+                }
+            case 4:
+                switch (enemy.Level)
+                {
+                    case 1:
+                        return 1.5f;
+                    case 2:
+                        return 1.3f;
+                    case 3:
+                        return 1.15f;
+                    case 4:
+                        return 1f;
+                    default:
+                        return 0.85f;
+                }
+            case 5:
+                switch (enemy.Level)
+                {
+                    case 1:
+                        return 1.6f;
+                    case 2:
+                        return 1.5f;
+                    case 3:
+                        return 1.3f;
+                    case 4:
+                        return 1.15f;
+                    default:
+                        return 1f;
+                }
+            default:
+                return 1;
+        }
+    }
 
 
-//    public override string ToString()
-//    {
-//        return String.Format(Name + "\tHP:\t{0}\t\tModifier:\t{1}", HealthPoints, AttackModifier);
-//    }
-//}
+    void Start()
+    {
+        HealthPoints = 1;
+        AttackPower = 0.1f;
+        AttackModifier = 1;
+    }
+
+
+    public override string ToString()
+    {
+        return String.Format(Name + "\tHP:\t{0}\t\tModifier:\t{1}", HealthPoints, AttackModifier);
+    }
+}
