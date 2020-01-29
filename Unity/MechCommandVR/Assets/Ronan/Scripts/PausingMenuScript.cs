5a1401aa352dff6db07116772037bd56ab957af6
Ronan Cassidy <36006416+S00177754@users.noreply.github.com>
2020-01-29 16:05:34 +0000

    Folder Cleanup And Menu Prefabs

---
+++ Unity/MechCommandVR/Assets/Ronan/Scripts/PausingMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausingMenuScript : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        Time.timeScale = 1;
+    }
+
+    private void OnDisable()
+    {
+        Time.timeScale = 0;
+    }
+
+
+}
