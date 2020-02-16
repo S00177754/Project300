5517a2f22e456488ec739ccc2d3484e2f348ae02
Ronan Cassidy <36006416+S00177754@users.noreply.github.com>
2020-02-16 13:29:47 +0000

    Designed & Implemented Buillding and Resource panel
    -Panel displays resource
    -Panel displays units and buildings
    -Auto generates details via commander

---
+++ Unity/MechCommandVR/Assets/Ronan/Scripts/CommandCenter/BuildingDetailPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class BuildingDetailPanel : MonoBehaviour
+{
+    public TMP_Text BuildingName;
+    public TMP_Text BuildingCount;
+    public Image BuildingSprite;
+
+   public void SetInfo(string name, string count, Sprite sprite)
+   {
+        BuildingSprite.sprite = sprite;
+        BuildingName.text = name;
+        BuildingCount.text = "X" + count;
+   }
+
+    public void UpdateCount(string count)
+    {
+        BuildingCount.text = "Count: X" + count;
+    }
+
+
+}
---
+++ Unity/MechCommandVR/Assets/Ronan/Scripts/CommandCenter/BuildingSelectionPanel.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BuildingSelectionPanel : MonoBehaviour
+{
+    public RectTransform content;
+    public GameObject BuildingPanelPrefab;
+    public BaseController baseController;
+    public TMP_Text resourceCount;
+
+    [Header("Sprites")]
+    public Sprite ResourceCollectorSprite;
+    public Sprite BarracksSprite;
+    public Sprite UnitSprite;
+
+    private BuildingDetailPanel resourcePanel;
+    private BuildingDetailPanel barracksPanel;
+    private BuildingDetailPanel unitPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateInfo();
+    }
+
+    private void GenerateList()
+    {
+        if (baseController != null)
+        {
+            resourceCount.text = "Resources: " + baseController.Owner.Resources;
+
+            GameObject panel = Instantiate(BuildingPanelPrefab, content);
+            resourcePanel = panel.GetComponent<BuildingDetailPanel>();
+            resourcePanel.SetInfo("Resource Collectors", baseController.ResourceCollectors.Count.ToString(), ResourceCollectorSprite);
+
+            panel = Instantiate(BuildingPanelPrefab, content);
+            barracksPanel = panel.GetComponent<BuildingDetailPanel>();
+            barracksPanel.SetInfo("Unit Barracks", baseController.UnitBarracks.Count.ToString(), BarracksSprite);
+
+            panel = Instantiate(BuildingPanelPrefab, content);
+            unitPanel = panel.GetComponent<BuildingDetailPanel>();
+            unitPanel.SetInfo("Units", baseController.Owner.Units.Count.ToString(), UnitSprite);
+        }
+    }
+
+    private void UpdateInfo()
+    {
+        if (baseController != null)
+        {
+            resourceCount.text = "Resources: " + baseController.Owner.Resources;
+            resourcePanel.UpdateCount(baseController.ResourceCollectors.Count.ToString());
+            barracksPanel.UpdateCount(baseController.UnitBarracks.Count.ToString());
+            unitPanel.UpdateCount(baseController.Owner.Units.Count.ToString());
+        }
+    }
+}
