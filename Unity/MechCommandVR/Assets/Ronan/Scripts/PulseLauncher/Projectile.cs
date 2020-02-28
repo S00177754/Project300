ba3ca24d74db9307ab8f3d6177b2b3f19d0deec0
Ronan Cassidy <36006416+S00177754@users.noreply.github.com>
2020-02-28 00:16:38 +0000

    Pulse Launcher Near Completion
    -Need to fix rotation launch issue
    -Add trail so its obvious where theyre going
    -Increase force of shot
    -Despawn timer not working

--- Unity/MechCommandVR/Assets/Ronan/Scripts/PulseLauncher/Projectile.cs
+++ Unity/MechCommandVR/Assets/Ronan/Scripts/PulseLauncher/Projectile.cs
@@ -2,17 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+public interface IDamageOutput
 {
-    // Start is called before the first frame update
-    void Start()
+    float DamageAmount { get; set; }
+}
+
+public class Projectile : MonoBehaviour, IDamageOutput
+{
+    public float DespawnTime = 5f;
+    public float LaunchForce = 5f;
+    public float Damage = 0.1f;
+
+    public float DamageAmount { get { return Damage; } set { Damage = value; } }
+
+    private Rigidbody rigidbody = null;
+    private float despawnTimer = 0f;
+    private bool isLaunched = false;
+
+
+    private void Awake()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        Destroy(this);
     }
+
+    private void Update()
+    {
+        if (isLaunched)
+        {
+            if(despawnTimer >= DespawnTime)
+            {
+                Destroy(this);
+            }
+            else
+            {
+                despawnTimer += (Time.deltaTime);
+            }
+        }
+    }
+
+    public void Launch(PulseLauncher blaster)
+    {
+        transform.position = blaster.LaunchPoint.position;
+        transform.rotation = blaster.transform.rotation;
+
+        rigidbody.AddRelativeForce(Vector3.forward * 10, ForceMode.Impulse);
+
+        isLaunched = true;
+    }
+
 }
--- Unity/MechCommandVR/Assets/Ronan/Scripts/PulseLauncher/PulseLauncher.cs
+++ Unity/MechCommandVR/Assets/Ronan/Scripts/PulseLauncher/PulseLauncher.cs
@@ -1,18 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class PulseLauncher : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("SteamVR")]
+    public SteamVR_Action_Boolean FireAction = null;
+
+    [Header("Blaster Details")]
+    public Transform LaunchPoint;
+
+    [Header("Ammo Clip")]
+    public List<GameObject> AmmoList;
+    public PulseClip Clip;
+
+    private SteamVR_Behaviour_Pose handPose = null;
+
+    private void Awake()
     {
-        
+        Clip = new PulseClip();
+        handPose = GetComponentInParent<SteamVR_Behaviour_Pose>();
+
+        if (AmmoList != null)
+            AmmoList.ForEach(a => Clip.AddToClip(a));
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (FireAction.GetStateDown(handPose.inputSource))
+            ShootProjectile();
     }
+
+    public void ShootProjectile()
+    {
+        GameObject projectile = Clip.TakeAmmo();
+        if (projectile != null)
+        {
+            projectile = Instantiate(projectile, LaunchPoint.position,Quaternion.identity);
+            projectile.GetComponent<Projectile>().Launch(this);
+        }
+    }
+}
+
+public class PulseClip
+{
+    public Queue<GameObject> Ammo;
+    public int ClipCapacity = 50;
+
+    public PulseClip()
+    {
+        Ammo = new Queue<GameObject>();
+    }
+
+    public bool AddToClip(GameObject ammunition)
+    {
+        Component projectileComponent;
+
+        if (Ammo.Count >= ClipCapacity)
+        {
+            return false;
+        }
+        
+        if(ammunition.TryGetComponent(typeof(Projectile),out projectileComponent))
+        {
+            //Add projectile component
+            Ammo.Enqueue(ammunition);
+            return true;
+        }
+
+        return false;
+    }
+
+    public GameObject TakeAmmo()
+    {
+        if(Ammo.Count <= 0)
+        {
+            return null;
+        }
+        else
+        {
+            return Ammo.Dequeue();
+        }
+    }
+
+    public List<GameObject> EmptyClip()
+    {
+        List<GameObject> Clip = new List<GameObject>();
+        foreach (var item in Ammo)
+        {
+            Clip.Add(item);
+            Ammo.Dequeue();
+        }
+        return Clip;
+    }
+    
 }
