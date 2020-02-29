608e1e0dc590d7370e08c50a42b02787bc7eee6f
Ronan Cassidy <36006416+S00177754@users.noreply.github.com>
2020-02-29 03:13:13 +0000

    Animations Fixed and applied IK to model

--- Unity/MechCommandVR/Assets/Ronan/Scripts/UnitAnimationController.cs
+++ Unity/MechCommandVR/Assets/Ronan/Scripts/UnitAnimationController.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class UnitAnimationController : MonoBehaviour
 {
-    private Animator animator;
+    public Animator animator;
 
     private void Start()
     {
-        animator = GetComponent<Animator>();
     }
 
    
