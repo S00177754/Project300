70b226b93edf987d71ab930454309e1457091fbf
Ronan Cassidy <36006416+S00177754@users.noreply.github.com>
2020-03-11 01:12:59 +0000

    Notification Additions & Build Settings

--- Unity/MechCommandVR/Assets/Ronan/Scripts/CommandCenter/NotificationController.cs
+++ Unity/MechCommandVR/Assets/Ronan/Scripts/CommandCenter/NotificationController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum NotificationSprites { Warning, Build }
+
 public class NotificationController : MonoBehaviour
 {
     public RectTransform Content;
     public GameObject NotificationPrefab;
+
+    [Header("Notification Sprites")]
     public Sprite WarningIcon;
+    public Sprite BuildIcon;
 
     public void UnitDestroyed(string unitName)
     {
@@ -15,8 +20,21 @@ public class NotificationController : MonoBehaviour
         notification.GetComponent<NotificationElement>().SetNotification(unitName+" Destroyed",WarningIcon,Color.red);
     }
 
-    public void SendNotification(string message, Sprite sprite, Color color)
+    public void SendNotification(string message, NotificationSprites value, Color color)
     {
+        Sprite sprite;
+        switch (value)
+        {
+            case NotificationSprites.Build:
+                sprite = WarningIcon;
+                break;
+
+            default:
+            case NotificationSprites.Warning:
+                sprite = BuildIcon;
+                break;
+        }
+
         GameObject notification = Instantiate(NotificationPrefab, Content);
         notification.GetComponent<NotificationElement>().SetNotification(message, sprite, color);
     }
